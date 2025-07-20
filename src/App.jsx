552a104b0f5a9c41f52559d4c3a0e5cbfceba9bc
mohamed <mohammedadell496@gmail.com>
2025-07-20 14:49:54 +0300

    add sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,24 @@ import Homepage from "./pages/Homepage";
 import Pricing from "./pages/Pricing";
 import Product from "./pages/Product";
 import Login from "./pages/Login";
+import AppLayout from "./pages/AppLayout";
 import PageNotFound from "./pages/PageNotFound";
+import City from "./components/City";
 
 function App() {
   return (
     <div className="">
       <Routes>
-        <Route path="/" element={<Homepage />} />
+        <Route index element={<Homepage />} />
         <Route path="pricing" element={<Pricing />} />
         <Route path="product" element={<Product />} />
         <Route path="login" element={<Login />} />
+        <Route path="app" element={<AppLayout />}>
+          <Route index element={<p>hello cities</p>} />
+          <Route path="cities" element={<p>hello cities</p>} />
+          <Route path="countries" element={<p>hello countries</p>} />
+          <Route path="form" element={<p>hello form</p>} />
+        </Route>
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </div>
